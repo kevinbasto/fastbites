0717966bc8e8722517b32538c6c6c6de7ba1d85d
kevin <bastokevindaniel@gmail.com>
2024-11-02 13:40:31 -0600

    feat: add client component

--- src/app/layouts/client/client.module.ts
+++ src/app/layouts/client/client.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
+import { ClientComponent } from './client.component';
 
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ClientComponent
+  ],
   imports: [
     CommonModule,
-    ClientRoutingModule
+    ClientRoutingModule,
+    MatSidenavModule,
+    MatButtonModule
   ]
 })
 export class ClientModule { }
