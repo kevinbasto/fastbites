53961c24d8217d953c83aa71cf312117ad5da4e9
kevinglydetalent <kevin.basto@glydetalent.com>
2024-11-03 09:20:10 -0600

    feat: add basic sketch of the recover component

--- src/app/pages/auth/recover/recover.component.ts
+++ src/app/pages/auth/recover/recover.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RecoverService } from './recover.service';
 
 @Component({
   templateUrl: './recover.component.html',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class RecoverComponent {
 
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private recoverService: RecoverService
+  ) {
+    this.form = this.fb.group({
+      email: [""],
+    });
+  }
+
+  login() {
+    let data = this.form.value;
+    this.recoverService
+  }
 }
