469819ad815deb7b83fca322cc5d775e33e262e4
kevinglydetalent <kevin.basto@glydetalent.com>
2024-11-02 21:30:41 -0600

    feat: add forms

---
+++ src/app/forms/recover-form/recover-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recover-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './recover-form.component.html',
+  styleUrl: './recover-form.component.scss'
+})
+export class RecoverFormComponent {
+
+}
