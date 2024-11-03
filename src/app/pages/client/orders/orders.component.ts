41f03f5792168e4fcdc6ed1f394e27d1b5cc5800
kevinglydetalent <kevin.basto@glydetalent.com>
2024-11-03 15:23:56 -0600

    feat: fb

--- src/app/pages/client/orders/orders.component.ts
+++ src/app/pages/client/orders/orders.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
-  standalone: true,
-  imports: [],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss'
 })
--- src/app/pages/client/orders/orders.module.ts
+++ src/app/pages/client/orders/orders.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+import { OrdersComponent } from './orders.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OrdersComponent
+  ],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    MatButtonModule
   ]
 })
 export class OrdersModule { }
