0f8ac302247bd1a4ee149b9a19d0b36d0b046a7b
kevinglydetalent <kevin.basto@glydetalent.com>
2024-11-03 08:46:42 -0600

    feat: add register basic sketch

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RegisterService } from './register.service';
 
 @Component({
   templateUrl: './register.component.html',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private register: RegisterService
+  ) {
+    this.form = this.fb.group({
+      email: [""],
+      password: [""],
+      confirmPassword: [""]
+    });
+  }
+
+  login() {
+    let data = this.form.value;
+    // this.register.signWithEmailAndPassword(data.email, data.password);
+  }
 }
