22603b45ea8ef1bbac1ff0851a57ecffcd50c28b
kevinglydetalent <kevin.basto@glydetalent.com>
2024-11-04 17:36:23 -0600

    feat: add mat card

--- src/app/pages/client/products/products.module.ts
+++ src/app/pages/client/products/products.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsComponent } from './products.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from "@angular/material/table";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductsComponent
+  ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    MatCardModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDialogModule
   ]
 })
 export class ProductsModule { }
