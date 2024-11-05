2f61614ac2a8cc7847642fc3110a1e43dd0b8bcc
kevinglydetalent <kevin.basto@glydetalent.com>
2024-11-05 04:38:45 -0600

    feat: add shared dependencies module and its components

---
+++ src/app/shared-components/shared-components.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableComponent } from './table/table.component';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from "@angular/material/menu";
+import { MatPaginatorModule } from "@angular/material/paginator";
+
+
+
+@NgModule({
+  declarations: [
+    TableComponent
+  ],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatPaginatorModule
+  ]
+})
+export class SharedComponentsModule { }
---
+++ src/app/shared-components/table/table.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { TableColumn } from '../../core/entities/table-column';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent {
+
+  //table inputs
+  @Input() title? : string;
+  @Input() data? : Array<any>;
+  @Input() headers? : Array<TableColumn>;
+  @Input() setSize : number = 0;
+
+  pageSize = 10
+
+  // table outputs
+  @Output() create : EventEmitter<null> = new EventEmitter();
+  @Output() edit : EventEmitter<any> = new EventEmitter();
+  @Output() deleteItem : EventEmitter<any> = new EventEmitter();
+  @Output() pageEvent : EventEmitter<PageEvent> = new EventEmitter();
+
+  displayedColumns: string[] = [];
+  dataSource : Array<any> = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['headers'])
+      this.setHeaders()
+
+    if(changes['data'])
+      this.dataSource = this.data!
+  }
+
+  setHeaders(){
+    this.displayedColumns = [];
+    for(let header of this.headers!)
+      this.displayedColumns.push(header.name);
+    this.displayedColumns.push('options')
+  }
+
+
+}
