610d0342951b29aebefef1c73fdd37cd78463955
kevinglydetalent <bastokevindaniel@gmail.com>
2024-11-07 17:23:40 -0600

    feat: add products

---
+++ src/app/pages/client/products/products-table.headers.ts
@@ -0,0 +1,14 @@
+export const productTableHeaders = [
+    {
+      name: "name",
+      displayName: "Nombre"
+    },
+    {
+      name: "description",
+      displayName: "Descripción"
+    },
+    {
+      name: "price",
+      displayName: "Precio"
+    },
+  ]
--- src/app/pages/client/products/products.component.ts
+++ src/app/pages/client/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TableColumn } from '../../../core/entities/table-column';
 import { ProductsService } from './products.service';
+import { productTableHeaders } from './products-table.headers';
 
 @Component({
   templateUrl: './products.component.html',
@@ -10,20 +11,7 @@ export class ProductsComponent {
 
   title : string = "Listado de productos";
   data : Array<any> = [];
-  headers : Array<TableColumn> = [
-    {
-      name: "name",
-      displayName: "Nombre"
-    },
-    {
-      name: "description",
-      displayName: "Descripción"
-    },
-    {
-      name: "price",
-      displayName: "Precio"
-    },
-  ];
+  headers : Array<TableColumn> = productTableHeaders;
   size: number = 0;
 
   constructor(
