e2eb16d2f280aa23e2a6859e7232b171bedfe42c
kevinglydetalent <bastokevindaniel@gmail.com>
2024-11-07 22:59:41 -0600

    feat: define the user and product structures

--- src/app/core/entities/product.ts
+++ src/app/core/entities/product.ts
@@ -1,2 +1,8 @@
 export interface Product {
+    name: string;
+    description: string;
+    cost: number;
+    price: number;
+    available: boolean;
+    imageUrl: string;
 }
--- src/app/core/entities/user.ts
+++ src/app/core/entities/user.ts
@@ -1,2 +1,7 @@
 export interface User {
+    email: string;
+    uid: string;
+    verified: boolean;
+    terms: boolean;
+    creationDate: number | Date;
 }
