4a9979c3eb0492a6919a7cfb1770e93b9a677c91
kevin <bastokevindaniel@gmail.com>
2024-11-08 08:44:43 -0600

    feat: include the currency mask in the form

--- src/app/shared-forms/shared-forms.module.ts
+++ src/app/shared-forms/shared-forms.module.ts
@@ -4,6 +4,8 @@ import { ProductFormComponent } from './product-form/product-form.component';
 import { FormActionsComponent } from './form-actions/form-actions.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { CurrencyMaskModule } from "ng2-currency-mask";
+
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -20,11 +22,14 @@ import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    
+    CurrencyMaskModule,
+
     MatInputModule,
     MatSlideToggleModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
   ],
   exports: [
     ProductFormComponent
