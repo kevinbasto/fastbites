741e072509f748f89e6ce026873202fd8cc14f84
kevin <bastokevindaniel@gmail.com>
2024-11-08 09:16:55 -0600

    feat: add create product

--- src/app/pages/client/products/pages/create-product/create-product.service.ts
+++ src/app/pages/client/products/pages/create-product/create-product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { AuthService } from '../../../../../core/services/auth/auth.service';
 import { Router } from '@angular/router';
+import { Product } from '../../../../../core/entities/product';
+import { SnackbarService } from '../../../../../core/services/snackbar/snackbar.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +13,19 @@ export class CreateProductService {
   constructor(
     private firestore: Firestore,
     private auth: AuthService,
-    private router : Router
+    private router : Router,
+    private snackbar: SnackbarService
   ) { }
 
   goBack(){
     this.router.navigate([`/client/products`]);
   }
 
-  createProduct() {}
+  async createProduct(product: Product) {
+    try {
+      let uid = await this.auth.getUID();
+    } catch (error) {
+      
+    }
+  }
 }
