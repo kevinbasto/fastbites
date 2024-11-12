c36be172e1e3f7cdb6d8f07ec7b45b3be7d2b588
kevin <bastokevindaniel@gmail.com>
2024-11-11 21:20:19 -0600

    feat: adjustment

--- src/app/pages/client/products/pages/edit-product/edit-product.component.ts
+++ src/app/pages/client/products/pages/edit-product/edit-product.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EditProductService } from './edit-product.service';
+import { CroppedImage } from '../../../../../core/generics/cropped-image';
+import { Product } from '../../../../../core/entities/product';
 
 @Component({
   templateUrl: './edit-product.component.html',
   styleUrl: './edit-product.component.scss'
 })
-export class EditProductComponent {
+export class EditProductComponent implements OnInit{
   
+  uploading: boolean = false;
+  file?: File;
+  cropped?: CroppedImage;
+  rawUrl?: string = '';
+
   constructor(
-    editProductServ: EditProductService
-  ) {}
-  
+    private editProductServ: EditProductService
+  ) { }
+
+  ngOnInit(): void {
+    let data : Product = JSON.parse(window.localStorage.getItem("editProduct")!);
+    // this.rawUrl = data.rawImage;
+  }
+
+  cancel() {
+    this.editProductServ.goBack();
+  }
+
+  createProduct(product: Product) {
+    this.uploading = !this.uploading;
+    // this.editProductServ.createProduct(product, this.file!, this.cropped!)
+    //   .finally(() => {
+    //     this.uploading = !this.uploading;
+    //   })
+  }
+
 }
--- src/app/pages/client/products/pages/edit-product/edit-product.service.ts
+++ src/app/pages/client/products/pages/edit-product/edit-product.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { AuthService } from '../../../../../core/services/auth/auth.service';
+import { Storage, getBytes, ref } from '@angular/fire/storage';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../../../../../core/services/snackbar/snackbar.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditProductService {
 
-  constructor() { }
+  constructor(
+    private firestore: Firestore,
+    private auth: AuthService,
+    private storage: Storage,
+    private router : Router,
+    private snackbar: SnackbarService
+  ) { }
+
+  goBack(){
+    this.router.navigate([`/client/products`]);
+  }
+
+  async fetchImage(uuid : string) {
+    try {
+      let uid = await this.auth.getUID();
+      let docRef = ref(this.storage, `/${uuid}/${uid}/${uuid}-raw.jpg`);
+      getBytes(docRef);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
