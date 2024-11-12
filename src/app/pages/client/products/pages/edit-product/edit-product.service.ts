c24760dc51f8a747d78fc121e90d4e0ebc5e8104
kevin <bastokevindaniel@gmail.com>
2024-11-12 10:12:16 -0600

    feat: add edit product basic functionality

--- src/app/pages/client/products/pages/edit-product/edit-product.service.ts
+++ src/app/pages/client/products/pages/edit-product/edit-product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 import { AuthService } from '../../../../../core/services/auth/auth.service';
-import { Storage, getBytes, ref } from '@angular/fire/storage';
+import { Storage, getBytes, getDownloadURL, ref, uploadBytes } from '@angular/fire/storage';
 import { Router } from '@angular/router';
 import { SnackbarService } from '../../../../../core/services/snackbar/snackbar.service';
+import { NgxImageCompressService } from 'ngx-image-compress';
 
 @Injectable({
   providedIn: 'root'
@@ -15,20 +16,85 @@ export class EditProductService {
     private auth: AuthService,
     private storage: Storage,
     private router : Router,
-    private snackbar: SnackbarService
+    private snackbar: SnackbarService,
+    private imageCompressServ: NgxImageCompressService
   ) { }
 
   goBack(){
     this.router.navigate([`/client/products`]);
   }
 
-  async fetchImage(uuid : string) {
+  async updateProduct(){
     try {
-      let uid = await this.auth.getUID();
-      let docRef = ref(this.storage, `/${uuid}/${uid}/${uuid}-raw.jpg`);
-      getBytes(docRef);
+      
     } catch (error) {
       throw error;
     }
+  }
+
+  private async prepareImage(name: string, file: File) {
+    try {
+      let format = file.name.split(".")[file.name.split(".").length - 1];
+      let buffer = await file.arrayBuffer();
+      return new File([buffer], `${name}.${format}`, {type: file.type});
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async storeImage(uid: string, prodUuid: string, file: File) : Promise<string> {
+    try {
+      
+      let url : string = "";
+      let fileRef = ref(this.storage, `/${uid}/${prodUuid}/${file.name}`);
+      await uploadBytes(fileRef, file);
+      url = await getDownloadURL(fileRef);
+      return url;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async getProducts(uid: string){
+    try {
+      let docRef = doc(this.firestore, `/users/${uid}/data/products`);
+      let products : any = (await getDoc(docRef)).data();
+      return products.products;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+  private async compressImage(file: File) {
+    try {
+      let base64 = await this.fileToBase64(file);
+      let compressed = await this.imageCompressServ.compressFile(base64, 1, 50, 50, 75, 75);
+      let blob = (await ((await fetch(compressed)).blob()))
+      let compressedFile = new File([blob], file.name, {type: file.type});
+      return compressedFile;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private fileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        
+        reader.onloadend = () => {
+            if (reader.result) {
+                resolve(reader.result.toString());
+            } else {
+                reject(new Error('Failed to convert file to Base64'));
+            }
+        };
+        
+        reader.onerror = () => {
+            reject(new Error('Error reading file'));
+        };
+        
+        reader.readAsDataURL(file);
+    });
   }
 }
