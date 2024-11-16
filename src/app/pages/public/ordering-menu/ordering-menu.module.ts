c1ffb6d4d27b0de5b87b72e2a6e0cad581e1382c
kevin <bastokevindaniel@gmail.com>
2024-11-15 18:47:09 -0600

    feat: added the imports of the module

--- src/app/pages/public/ordering-menu/ordering-menu.module.ts
+++ src/app/pages/public/ordering-menu/ordering-menu.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { OrderingMenuRoutingModule } from './ordering-menu-routing.module';
 import { OrderingMenuComponent } from './ordering-menu.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -11,7 +15,11 @@ import { OrderingMenuComponent } from './ordering-menu.component';
   ],
   imports: [
     CommonModule,
-    OrderingMenuRoutingModule
+    OrderingMenuRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatIconModule
   ]
 })
 export class OrderingMenuModule { }
