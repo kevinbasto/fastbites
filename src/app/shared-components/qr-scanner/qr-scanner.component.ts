f3dd50a830bf5e71008329bb60328bee2f371bd5
kevin <bastokevindaniel@gmail.com>
2024-11-20 10:02:21 -0600

    feat: add qr-scanner functionalities to the qr scanner

--- src/app/shared-components/qr-scanner/qr-scanner.component.ts
+++ src/app/shared-components/qr-scanner/qr-scanner.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import QrScanner from 'qr-scanner';
 
 @Component({
   selector: 'qr-scanner',
   templateUrl: './qr-scanner.component.html',
   styleUrl: './qr-scanner.component.scss'
 })
-export class QrScannerComponent {
+export class QrScannerComponent implements OnInit{
 
+  qrScanner!: QrScanner
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const videoElem = document.getElementById("viewport");
+    this.qrScanner = new QrScanner(
+      videoElem as any,
+      (result : any) => console.log('decoded qr code:', result),
+      {
+        highlightScanRegion: true,
+        highlightCodeOutline: true
+      },
+    );
+    this.qrScanner.start()
+  }
 }
