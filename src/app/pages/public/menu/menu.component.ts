5641e634a758815e94e93acf4e854385c31c5b31
kevin <bastokevindaniel@gmail.com>
2024-11-20 08:49:33 -0600

    feat: add suppor to the menu

--- src/app/pages/public/menu/menu.component.ts
+++ src/app/pages/public/menu/menu.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from './menu.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
 
+  constructor(
+    public menuService: MenuService
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
 }
