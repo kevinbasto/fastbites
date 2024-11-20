bd1c83bd5681e83545f9856ca735b24760d90ab7
kevinglydetalent <bastokevindaniel@gmail.com>
2024-11-20 06:17:24 -0600

    feat: add settings component basic imports

--- src/app/pages/client/settings/settings.component.ts
+++ src/app/pages/client/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SettingsService } from './settings.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+
+  constructor(
+    private settingsService: SettingsService
+  ) {}
 
 }
--- src/app/pages/client/settings/settings.module.ts
+++ src/app/pages/client/settings/settings.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { SettingsComponent } from './settings.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SharedComponentsModule } from '../../../shared-components/shared-components.module';
+import { SharedFormsModule } from '../../../shared-forms/shared-forms.module';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SettingsComponent
+  ],
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    MatCardModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatInputModule,
+    SharedComponentsModule,
+    SharedFormsModule
   ]
 })
 export class SettingsModule { }
