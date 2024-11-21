c156198356bb63396a247c09da351d1710b4d678
kevinglydetalent <bastokevindaniel@gmail.com>
2024-11-21 16:04:25 -0600

    feat: add menu fetching of the product

--- src/app/pages/client/menu/menu.component.ts
+++ src/app/pages/client/menu/menu.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from './menu.service';
+import { Product } from '../../../core/entities/product';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+
+  products?: Array<Product>;
+
+  constructor(
+    private menuServ: MenuService
+  ) {}
+
+  ngOnInit(): void {
+    this.menuServ.fetchProducts()
+    .subscribe(products => {
+      this.products = products;
+    })
+  }
 
 }
--- src/app/pages/client/menu/menu.service.ts
+++ src/app/pages/client/menu/menu.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { ProductsRepoService } from '../../../core/repos/products-repo/products-repo.service';
+import { Observable } from 'rxjs';
+import { Product } from '../../../core/entities/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private productsRepo: ProductsRepoService
+  ) { }
+
+  fetchProducts() {
+    return new Observable<Array<Product>>((obs) => {
+      this.auth.getUID()
+      .then((uid) => {
+        this.productsRepo.fetchProducts(uid!)
+        .subscribe(products => {
+          if(!products)            
+            obs.next([])
+          else
+            obs.next(products)
+        })
+      }).catch((err) => {
+        obs.error("error")
+      });
+    });
+  }
 }
