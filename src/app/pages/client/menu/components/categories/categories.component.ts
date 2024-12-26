ef1c47796ebeb0bf900896333f66eb1368c1c2a4
kevin <bastokevindaniel@gmail.com>
2024-12-26 14:09:29 -0600

    feat: add categories and menu component

--- src/app/pages/client/menu/components/categories/categories.component.ts
+++ src/app/pages/client/menu/components/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { categoriesTableConfig, categoriesTableHeaders } from '../../menu-table.headers';
+import { Category } from '../../../../../core/entities/category';
 
 @Component({
   selector: 'app-categories',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class CategoriesComponent {
 
+  @Input() categories?: Array<Category>;
+  categoriesHeaders = categoriesTableHeaders;
+  categoriesConfig = categoriesTableConfig;
+
+  createCategory() {
+    // this.productsService.createCategory()
+    // .then((result) => this.fetchMenu());
+  }
+
+  viewCategory(category: Category) {
+    // this.productsService.viewCategory(category);
+  }
+
+  editCategory(category: Category) {
+    // this.productsService.editCategory(category)
+    // .then((result) => this.fetchMenu());
+  }
+
+  deleteCategory(category : Category) {
+    // this.productsService.deleteCategory(category)
+    // .then(() => this.fetchMenu());
+  }
 }
--- src/app/pages/client/menu/components/products/products.component.ts
+++ src/app/pages/client/menu/components/products/products.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductsService } from './products.service';
+import { categoriesTableConfig, productTableHeaders } from '../../menu-table.headers';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Category } from '../../../../../core/entities/category';
+import { Router } from '@angular/router';
+import { Product } from '../../../../../core/entities/product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  form: FormGroup
+
+  @Input() products?: Array<Product>;
+  headers = productTableHeaders;
+  tableConfig = categoriesTableConfig;
+  categories: Array<Category> = [];
+  
+
+  constructor(
+    private productsService: ProductsService,
+    private fb: FormBuilder,
+    private router: Router,
+  ) {
+    this.form = this.fb.group({
+      category: [""]
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  viewQrDialog() {
+    // this.productsService.viewQrDialog();
+  }
+
+  importProductsFromFile() {
+    // this.productsService.importProductsFromFile()
+    // .then((result) => {
+    //   this.fetchMenu();
+    // }).catch((err) => {
+      
+    // });
+  }
+
+  createProduct() {
+    this.router.navigate(['/client/products/create']);
+  }
+
+  editProduct(product: Product) {
+    this.router.navigate([`/client/products/${product.id}`]);
+  }
+
+  viewProduct(product: Product) {
+    // this.productsService.viewProduct(product);
+  }
+
+  deleteProduct(product: Product) {
+    // this.productsService.deleteProduct(product)
+      // .then((result) => this.fetchMenu());
+  }
+
+  toggleProduct(product: Product) {
+    // this.productsService.toggleProduct(product);
+  }
 
 }
