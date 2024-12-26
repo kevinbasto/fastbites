e44e37e651c3216c467900ca0bb7576c926179ff
kevin <bastokevindaniel@gmail.com>
2024-12-26 12:34:52 -0600

    feat: add image formatting to make it readable

--- script.sh
+++ script.sh
@@ -1,3 +1,1 @@
-ng g s pages/client/products/routes/edit-submenu/edit-submenu;
-git add .;
-git commit -m "feat: recreate the edit submenus service";
+ng g c pages/client/products/components/products
