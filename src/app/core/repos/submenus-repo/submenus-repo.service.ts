ab6233e5a4ece0520b7090e878b5b8e2172ca7d2
kevin <bastokevindaniel@gmail.com>
2025-01-13 13:57:09 -0600

    feat: add submenus

--- src/app/core/repos/submenus-repo/submenus-repo.service.ts
+++ src/app/core/repos/submenus-repo/submenus-repo.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { MenuRepoService } from '../menu-repo/menu-repo.service';
+import { AuthService } from '../../services/auth/auth.service';
+import { Submenu } from '../../entities/submenu';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubmenusRepoService {
 
-  constructor() { }
+  uid!: string;
+
+  constructor(
+    private menuRepo: MenuRepoService,
+    private authServ: AuthService
+  ) { }
+
+  async createsubmenu(submenu: Submenu) {
+    try {
+      let uid = await this.authServ.getUID();
+      let submenus = (await this.menuRepo.fetchMenu(uid))?.submenus;
+      submenus?.push(submenu);
+      await this.menuRepo.updateMenu(uid, { submenus });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updatesubmenu(submenu: Submenu) {
+    try {
+      let uid: string = await this.authServ.getUID();
+      let submenus = (await this.menuRepo.fetchMenu(uid))?.submenus;
+      submenus = submenus?.map((sub: Submenu) => {
+        if (submenu.id == submenu.id) sub = submenu;
+        return sub;
+      });
+      await this.menuRepo.updateMenu(uid, { submenus });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deletesubmenu(submenu: Submenu) {
+    try {
+      let uid = await this.authServ.getUID();
+      let submenus = (await this.menuRepo.fetchMenu(uid))?.submenus;
+      submenus = submenus?.filter((submenu: Submenu) => submenu.id != submenu.id);
+      await this.menuRepo.updateMenu(uid, { submenus });
+    } catch (error) {
+      throw error;
+    }
+  }
 }
