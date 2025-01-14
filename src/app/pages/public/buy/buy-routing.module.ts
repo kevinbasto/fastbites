5c9ec1a0775a8e3be22dd3f506d8b2efa2e12e8a
kevin <bastokevindaniel@gmail.com>
2025-01-14 17:56:55 -0600

    feat: add routing to the children modules

--- src/app/pages/public/buy/buy-routing.module.ts
+++ src/app/pages/public/buy/buy-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuyComponent } from './buy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: BuyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
