e5ba903ed67c317515b52c84f0d90f7929428ee8
kevin <bastokevindaniel@gmail.com>
2025-01-14 07:20:25 -0600

    feat: add api render on master

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     domain: window.origin,
-    apiUrl: "http://localhost:5001/fastbites-321c9/us-central1/api",
+    apiUrl: "https://fastbites-api.onrender.com",
     paginationOptions : [ 10, 20, 50 ],
     defaultPageSize: 10
 };
