38244d74f38452ad291a81fe3d1409e6fafe7137
kevin <bastokevindaniel@gmail.com>
2025-01-14 17:55:15 -0600

    feat: add routing to fastbites

--- src/app/pages/public/public-routing.module.ts
+++ src/app/pages/public/public-routing.module.ts
@@ -14,6 +14,22 @@ const routes: Routes = [
   {
     path: "menu",
     loadChildren: () => import("./menu/menu.module").then(m => m.MenuModule)
+  },
+  {
+    path: "buy",
+    loadChildren: () => import('./buy/buy.module').then(m => m.BuyModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path: 'book',
+    loadChildren: () => import('./book/book.module').then(m => m.BookModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./documentation/documentation.module').then(m => m.DocumentationModule)
   }
 ];
 
