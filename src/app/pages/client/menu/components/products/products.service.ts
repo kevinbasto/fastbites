3ada3dfb818974a92488b9f2500bbb77e5df21b8
kevin <bastokevindaniel@gmail.com>
2025-01-13 22:38:41 -0600

    feat: add support category toggle and delete

--- src/app/pages/client/menu/components/products/products.service.ts
+++ src/app/pages/client/menu/components/products/products.service.ts
@@ -5,6 +5,7 @@ import { Product } from '../../../../../core/entities/product';
 import { ViewProductComponent } from '../../dialogs/view-product/view-product.component';
 import { Message } from '../../../../../core/generics/message';
 import { ConfirmDialogComponent } from '../../../../../shared-components/confirm-dialog/confirm-dialog.component';
+import { SnackbarService } from '../../../../../core/services/snackbar/snackbar.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,8 @@ export class ProductsService {
 
   constructor(
     private matDialog: MatDialog,
-    private productRepo: ProductsRepoService
+    private productRepo: ProductsRepoService,
+    private snackbar: SnackbarService,
   ) { }
 
   visualizeProduct(product: Product) {
@@ -26,10 +28,25 @@ export class ProductsService {
       message: 'Una vez hecha esta acción no se puede deshacer'
     };
     const dialog = this.matDialog.open(ConfirmDialogComponent, { data: { ...message } });
+    dialog.afterClosed().subscribe((confirmation: boolean) => {
+      if(!confirmation) return;
+      this.productRepo.deleteProduct(product)
+      .then((result) => {
+        this.snackbar.openMessage('Submenu borrado con éxito');
+      }).catch((err) => {
+        this.snackbar.openMessage('No se pudo borrar el submenú');
+      });
+    })
   }
 
   toggleProduct(product: Product) {
-
+    product.available = !product.available;
+    this.productRepo.updateProduct(product)
+    .then((result) => {
+      this.snackbar.openMessage('Producto actualizada con éxito');
+    }).catch((err) => {
+      this.snackbar.openMessage('No se pudo actualizar la Producto');
+    });
   }
   
 }
