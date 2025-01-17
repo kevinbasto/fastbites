5c667e7e5bd2ab2e6c73def7858633ac69edc01e
kevin <bastokevindaniel@gmail.com>
2025-01-17 13:26:00 -0600

    feat: add create-employee component and service

--- src/app/pages/client/staff/staff.module.ts
+++ src/app/pages/client/staff/staff.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { StaffRoutingModule } from './staff-routing.module';
 import { StaffComponent } from './staff.component';
 import { EmployeesComponent } from './components/employees/employees.component';
+import { CreateEmployeeComponent } from './routes/create-employee/create-employee.component';
 
 
 @NgModule({
   declarations: [
     StaffComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     CommonModule,
