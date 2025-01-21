d01b4135486a48e8b15d146b23a1ba171bb0633a
kevin <bastokevindaniel@gmail.com>
2025-01-21 10:40:05 -0600

    feat: add the new version counting variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
     domain: window.origin,
     apiUrl: "https://fastbites-api.onrender.com",
     paginationOptions : [ 10, 20, 50 ],
-    defaultPageSize: 10
+    defaultPageSize: 10,
+    version: '1.1.0',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     domain: window.origin,
     apiUrl: 'http://localhost:3000',
     paginationOptions : [ 10, 20, 50 ],
-    defaultPageSize: 10
+    defaultPageSize: 10,
+    version: '1.1.0',
 }
