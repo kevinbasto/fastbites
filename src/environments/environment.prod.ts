6b9918dbe78d306e8b1b477845c6fb9eecaec62d
kevin <bastokevindaniel@gmail.com>
2025-01-21 10:41:50 -0600

    feat: add version counting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
+import { version } from "./version";
+
 export const environment = {
     domain: window.origin,
     apiUrl: "https://fastbites-api.onrender.com",
     paginationOptions : [ 10, 20, 50 ],
     defaultPageSize: 10,
-    version: '1.1.0',
+    version: version,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
+import { version } from "./version";
+
 export const environment = {
     domain: window.origin,
     apiUrl: 'http://localhost:3000',
     paginationOptions : [ 10, 20, 50 ],
     defaultPageSize: 10,
-    version: '1.1.0',
+    version: version,
 }
