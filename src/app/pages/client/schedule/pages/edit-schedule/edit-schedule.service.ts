7d2fa4ea64615f5c2f25ec930496cab8391b57d2
kevin <bastokevindaniel@gmail.com>
2025-01-22 11:52:36 -0600

    feat: add edit a schedule modification

--- src/app/pages/client/schedule/pages/edit-schedule/edit-schedule.service.ts
+++ src/app/pages/client/schedule/pages/edit-schedule/edit-schedule.service.ts
@@ -22,6 +22,51 @@ export class EditScheduleService {
   };
 
   async updateSchedule(schedule: Schedule) {
+    if (schedule.available) {
+      const schedules = await this.schedulesrepo.getSchedules();
+      if (schedules) {
+        const filteredSchedules = schedules.filter(s => s.id !== schedule.id);
+        const isOverlapping = this.hasOverlaps([...filteredSchedules, schedule]);
+        if (isOverlapping) {
+          schedule.available = false;
+        }
+      }
+    }
     return this.schedulesrepo.updateSchedule(schedule);
+  }
+
+  parseTime(time: string): Date {
+    const [timePart, modifier] = time.split(' ');
+    let [hours, minutes] = timePart.split(':').map(Number);
+  
+    if (modifier === 'PM' && hours !== 12) {
+      hours += 12;
+    } else if (modifier === 'AM' && hours === 12) {
+      hours = 0;
+    }
+  
+    const date = new Date();
+    date.setHours(hours, minutes, 0, 0);
+    return date;
+  }
+  
+  hasOverlaps(schedules: Schedule[]): boolean {
+    const times = schedules.map(schedule => ({
+      opening: this.parseTime(schedule.openingHour),
+      closing: this.parseTime(schedule.closingHour)
+    }));
+  
+    for (let i = 0; i < times.length; i++) {
+      for (let j = i + 1; j < times.length; j++) {
+        if (
+          (times[i].opening < times[j].closing && times[i].closing > times[j].opening) ||
+          (times[j].opening < times[i].closing && times[j].closing > times[i].opening)
+        ) {
+          return true;
+        }
+      }
+    }
+  
+    return false;
   }
 }
