f33d32941d63e93adb68a117f677099565e4f25c
kevin <bastokevindaniel@gmail.com>
2025-01-31 22:05:57 -0600

    feat: add native builder

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'fastbites',
+  webDir: 'dist/fastbites/browser',
+  
+};
+
+export default config;
