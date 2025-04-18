ca2b0c234c55ad9faa37fc02669892f368edd58a
kevin <bastokevindaniel@gmail.com>
2025-04-18 13:01:43 -0600

    feat: add firebase deploy target

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,4 +6,7 @@ export const environment = {
     paginationOptions : [ 10, 20, 50 ],
     defaultPageSize: 10,
     version: version,
+    landingDomain: "fastbites.app",
+    adminDomain: "admin.fastbites.app",
+    menuDomain: "menu.fastbites.app",
 };
